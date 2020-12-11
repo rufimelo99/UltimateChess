53840b0c106e779d0027b0de2b6a63711a4997ab
Melo <rufimelo99@gmail.com>
2020-12-11 18:37:38 +0000

    InitialSpawning Handling

--- Assets/BoardManager.cs
+++ Assets/BoardManager.cs
@@ -5,11 +5,63 @@ using UnityEngine;
 public class BoardManager : MonoBehaviour
 {
     private const float TILE_SIZE = 1.0f;
-    private const float TILE_OFFSET = 0.1f;
 
+    //Tile Selected atm
     private int CurrentTileX = -1;
     private int CurrentTileZ = -1;
 
+    //pieces in the board
+    public List<GameObject> chessPieces = new List<GameObject>();   //only initialized so i dont have an error on the console (it would run either way)
+    public GameObject activeChessPiece;
+
+    private void Start()
+    {
+        //Spawn Black Towers
+        SpawnPiece(0, GetTileCenter(0, 7));
+        SpawnPiece(7, GetTileCenter(7, 7));
+        //Spawn Black Horses
+        SpawnPiece(1, GetTileCenter(1, 7));
+        SpawnPiece(6, GetTileCenter(6, 7));
+        //Spawn Black Bishops
+        SpawnPiece(2, GetTileCenter(2, 7));
+        SpawnPiece(5, GetTileCenter(5, 7));
+        //Spawn Black Queen and King
+        SpawnPiece(3, GetTileCenter(3, 7));
+        SpawnPiece(4, GetTileCenter(4, 7));
+        //Spawn Black Pawns
+        SpawnPiece(8, GetTileCenter(0, 6));
+        SpawnPiece(9, GetTileCenter(1, 6));
+        SpawnPiece(10, GetTileCenter(2, 6));
+        SpawnPiece(11, GetTileCenter(3, 6));
+        SpawnPiece(12, GetTileCenter(4, 6));
+        SpawnPiece(13, GetTileCenter(5, 6));
+        SpawnPiece(14, GetTileCenter(6, 6));
+        SpawnPiece(15, GetTileCenter(7, 6));
+
+        //Spawn White Pawns
+        SpawnPiece(16, GetTileCenter(0, 1));
+        SpawnPiece(17, GetTileCenter(1, 1));
+        SpawnPiece(18, GetTileCenter(2, 1));
+        SpawnPiece(19, GetTileCenter(3, 1));
+        SpawnPiece(20, GetTileCenter(4, 1));
+        SpawnPiece(21, GetTileCenter(5, 1));
+        SpawnPiece(22, GetTileCenter(6, 1));
+        SpawnPiece(23, GetTileCenter(7, 1));
+
+        //Spawn Black Towers
+        SpawnPiece(24, GetTileCenter(0, 0));
+        SpawnPiece(31, GetTileCenter(7, 0));
+        //Spawn Black Horses           
+        SpawnPiece(25, GetTileCenter(1, 0));
+        SpawnPiece(30, GetTileCenter(6, 0));
+        //Spawn Black Bishops          
+        SpawnPiece(26, GetTileCenter(2, 0));
+        SpawnPiece(29, GetTileCenter(5, 0));
+        //Spawn Black Queen and King   
+        SpawnPiece(27, GetTileCenter(3, 0));
+        SpawnPiece(28, GetTileCenter(4, 0));
+
+    }
     // Update is called once per frame
     void Update()
     {
@@ -23,10 +75,33 @@ public class BoardManager : MonoBehaviour
         }
         RaycastHit hit;
         //LayerMask.GetMask("ChessPlane") returns the int in layers in the chess board
-        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("ChessPlane"))) {
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("ChessPlane")))
+        {
             //where did the collision happened
             Debug.Log(hit.point);
-        }  
+            CurrentTileX = (int)hit.point.x;
+            CurrentTileZ = (int)hit.point.z;
+        }
+        else
+        {
+            CurrentTileX = -1;
+            CurrentTileZ = -1;
+        }
+    }
+    
+    //Spawn a Certain Piece from the List ChessPieces and gives it a position (objects inside list defined in the BoardManager on the UI inspector)
+    private void SpawnPiece(int index, Vector3 position) {
+        GameObject go = Instantiate(chessPieces[index], position, Quaternion.identity) as GameObject;
+        go.transform.SetParent(transform);
+        activeChessPiece=go;
+    }
+
+    private Vector3 GetTileCenter(int x, int z)
+    {
+        Vector3 origin = Vector3.zero;
+        origin.x += (TILE_SIZE * x) + TILE_SIZE/2;
+        origin.z += (TILE_SIZE * z) + TILE_SIZE/2;
+        return origin;
     }
 
 
@@ -45,9 +120,15 @@ public class BoardManager : MonoBehaviour
                 start = Vector3.right * j;
                 Debug.DrawLine(start, start +heigthLine );
 
-
-
             }
         }
+        //Draw Selection
+        if (CurrentTileX >= 0 && CurrentTileZ >= 0) {
+            Debug.DrawLine(
+                    Vector3.forward * CurrentTileZ + Vector3.right * CurrentTileX,
+                    Vector3.forward * (CurrentTileZ + 1) + Vector3.right * (CurrentTileX + 1)
+                );
+        }
+
     }
 }
---
+++ Assets/Tiny Chess Stone Chess Piece/Readme Eng.txt
@@ -0,0 +1,30 @@
+Content:
+
+1) Chess piece
+
+   - 6 white and 6 black ,stylized as a stone, chess pieces with 1k textures. + normal maps
+
+  Polygon: from 80 to 280 triangles on a chess piece
+
+2) Environment
+
+   - stylized as a stone, a chess battlefield with 1k textures. + normal maps
+
+ Polygon: 2,4 k tris
+
+3) Png Icon for chess piece
+
+All scene with 32 chess pieces and environment,using baking light - 10k triangles
+
+Size: 125mb - textures, 3mb - other
+
+ATTENTION! To display some elements (flags), you need a two-sided shader
+
+   I used the free Double Sided Shaders by CICONIA STUDIO.
+   Link: https://assetstore.unity.com/packages/vfx/shaders/double-sided-shaders-23087
+
+If you have any questions / suggestions / comments, etc., I will be happy to listen to you and try to help you!
+My email: 3boxassets@gmail.com
+Twitter:https://twitter.com/KliutkoCS
+
+Thank you!
