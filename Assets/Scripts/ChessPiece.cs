c159bcefbae4c7f92c964e404f5f8df1dc6f9025
Melo <rufimelo99@gmail.com>
2020-12-11 22:04:34 +0000

    .

---
+++ Assets/Scripts/ChessPiece.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ChessPiece : MonoBehaviour
+{
+    public int CurrentX  { set; get; }
+    public int CurrentZ { set; get; }
+    //dont know if ill need it.. depends on the implementation
+    public bool isWhite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
